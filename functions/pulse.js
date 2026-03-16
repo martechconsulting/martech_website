66c68680c393c40639798877db90b7d75a6d285f
martechconsulting <jon@martechconsulting.io>
2026-03-16 11:52:34 -0400

    Create pulse.js

---
+++ functions/pulse.js
@@ -0,0 +1,22 @@
+export async function onRequestPost({ request, env }) {
+  const body = await request.json();
+  const isPatch = body._method === 'PATCH';
+
+  const url = isPatch
+    ? `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/Martech%20Pulse%20Submissions/${body.recordId}`
+    : `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/Martech%20Pulse%20Submissions`;
+
+  const res = await fetch(url, {
+    method: isPatch ? 'PATCH' : 'POST',
+    headers: {
+      'Authorization': `Bearer ${env.AIRTABLE_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ fields: body.fields })
+  });
+
+  const data = await res.json();
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
