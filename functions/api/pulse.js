7ccc39b5745c0f195ae3bf38f3c0da77d9b0c393
martechconsulting <jon@martechconsulting.io>
2026-03-16 12:42:53 -0400

    Update pulse.js

--- functions/api/pulse.js
+++ functions/api/pulse.js
@@ -1,9 +1,12 @@
-export async function onRequestPost({ request, env }) {
+export async function onRequest({ request, env }) {
+  if (request.method !== 'POST') {
+    return new Response('Method not allowed', { status: 405 });
+  }
+
   try {
     const body = await request.json();
     const isPatch = body._method === 'PATCH';
-    const tableName = 'Martech%20Pulse%20Submissions';
-    const baseUrl = 'https://api.airtable.com/v0/' + env.AIRTABLE_BASE_ID + '/' + tableName;
+    const baseUrl = 'https://api.airtable.com/v0/' + env.AIRTABLE_BASE_ID + '/Martech%20Pulse%20Submissions';
     const url = isPatch ? baseUrl + '/' + body.recordId : baseUrl;
 
     const res = await fetch(url, {
